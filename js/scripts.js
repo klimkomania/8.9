de7aa4e64ce414074196ad195c4038b255fb5589
klimkomania <klimkomania@interia.pl>
2017-04-10 08:45:56 +0200

    add files

---
+++ js/scripts.js
@@ -0,0 +1,10 @@
+function drawTree(n) {
+	for ( var i = 0; i < n; i++) {
+		var star = "*";
+		for (var j = 0; j < i*2-1; j++) {
+			var star = star + "*";
+		}
+		console.log(star);
+	}
+}
+drawTree(10);
